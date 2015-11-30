fc66e7bd5acd9cc944ec6a6e3dbec06837e3fd19
niklasfuks <nikfuk-2@student.ltu.se>
2015-11-30 12:16:06 +0100

    Redesigned the view for posts
    Redesigned the view for posts , not done completly

--- modules/posts/client/controllers/post.client.controller.js
+++ modules/posts/client/controllers/post.client.controller.js
@@ -4,20 +4,25 @@ var postsApp = angular.module('posts');
 
 postsApp.controller('PostsController', ['$scope', '$state', 'Authentication', 'Posts',
     function ($scope, $state, Authentication, Posts) {
+
         $scope.authentication = Authentication;
 
-        $scope.listPosts = function(){
+        $scope.listPosts = function () {
             $scope.posts = Posts.query();
         };
 
-        $scope.$on('updateGetPosts', function (event,args) {
+        $scope.$on('updateGetPosts', function (event, args) {
             $scope.listPosts();
         });
+
     }]);
 
 postsApp.controller('PostsCreateController', ['$scope', '$state', 'Authentication', 'Posts',
     function ($scope, $state, Authentication, Posts) {
         $scope.authentication = Authentication;
+        $scope.left = function () {
+            return 150 - $scope.content.length;
+        };
 
         $scope.create = function () {
             var post = new Posts({
@@ -32,4 +37,25 @@ postsApp.controller('PostsCreateController', ['$scope', '$state', 'Authenticatio
             });
 
         };
+
     }]);
+
+postsApp.run(['$anchorScroll', function ($anchorScroll) {
+        $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
+    }])
+    .controller('headerCtrl', ['$anchorScroll', '$location', '$scope',
+        function ($anchorScroll, $location, $scope) {
+            $scope.gotoAnchor = function (x) {
+                var newHash = 'anchor' + x;
+                if ($location.hash() !== newHash) {
+                    // set the $location.hash to `newHash` and
+                    // $anchorScroll will automatically scroll to it
+                    $location.hash('anchor' + x);
+                } else {
+                    // call $anchorScroll() explicitly,
+                    // since $location.hash hasn't changed
+                    $anchorScroll();
+                }
+            };
+        }
+    ]);
