8380ec8f11fa0179d5f9f21493db2a56eaa5c5f4
niklasfuks <nikfuk-2@student.ltu.se>
2015-12-09 15:06:06 +0100

    for avoidning conflict

--- modules/posts/client/services/posts.client.service.js
+++ modules/posts/client/services/posts.client.service.js
@@ -2,11 +2,24 @@
 
 // Users service used for communicating with the users REST endpoint
 angular.module('posts').factory('Posts', ['$resource',
-    function ($resource) {
-        return $resource('api/posts', {}, {
-            create: {
-                method: 'POST'
-            }
-        });
-    }
-]);
+        function ($resource) {
+            return $resource('api/posts', {
+                postId: '@postId'
+            }, {
+                create: {
+                    method: 'POST'
+                },
+                upVote: {method: 'PUT', url: 'api/posts/:postId/upvote'}
+            });
+        }
+    ])
+    .factory('Post', ['$resource',
+        function ($resource) {
+            return $resource('api/posts/:postId', {
+                postId: '@postId'
+            }, {
+                upVote: {method: 'PUT', url: 'api/posts/:postId/upvote'},
+                downVote: {method: 'PUT', url: 'api/posts/:postId/downvote'}
+            });
+        }
+    ]);
