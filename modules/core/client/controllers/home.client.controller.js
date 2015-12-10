279a10eebe5fc8d19953c8212543ecad7e1988d5
niklasfuks <nikfuk-2@student.ltu.se>
2015-12-10 04:26:01 +0100

    No active location view

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,6 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.user = Authentication.user;
   }
 ]);
